125434f2a7fdfe48fb1251ba1402ed6c0b3a868d
t110368032HSU-ChienTe <93695115+t110368032HSU-ChienTe@users.noreply.github.com>
2022-07-19 17:16:42 +0800

    Add files via upload

--- cutAudio.py
+++ cutAudio.py
@@ -15,8 +15,8 @@ for speaker in speakers:
         os.mkdir("{}/{}".format(save_folder,speaker))
     wavs = os.listdir(data_folder+'/'+speaker)
     for wav in wavs:
-        if not os.path.exists("{}/{}/{}".format(save_folder,speaker,wav)):
-            os.mkdir("{}/{}/{}".format(save_folder,speaker,wav))
+        if not os.path.exists("{}/{}/{}".format(save_folder,speaker,wav[:-4])):
+            os.mkdir("{}/{}/{}".format(save_folder,speaker,wav[:-4]))
         print(wav)
         song = AudioSegment.from_wav(data_folder+'/'+speaker+'/'+wav)
         sec = song.duration_seconds
@@ -26,7 +26,7 @@ for speaker in speakers:
         print(sec,cat_sec)
         #是否低於60s
         if rule_times != 0:
-            print("rule_times")
+            
             #每隔60秒切割
             for part in range(rule_times):
                 #防止part=0
@@ -34,12 +34,12 @@ for speaker in speakers:
                 part+=1
                 print(part)
                 time = song[(part-1)*cat_sec*1000:(part)*cat_sec*1000]
-                data_split_filename=('{}/{}/{}_{}.wav'.format(save_folder,speaker,wav,part))
+                data_split_filename=('{}/{}/{}_{}.wav'.format(save_folder,speaker,wav[:-4],part))
                 time.export(data_split_filename,format="wav")
         #剩餘部分
         if rule_times_flout!=0:
             time = song[-rule_times_flout*1000:]
-            data_split_filename=('{}/{}/{}_{}.wav'.format(save_folder,speaker,wav,'lower_60'))
+            data_split_filename=('{}/{}/{}_{}.wav'.format(save_folder,speaker,wav[:-4],'lower_60'))
             time.export(data_split_filename,format="wav")
 
 
