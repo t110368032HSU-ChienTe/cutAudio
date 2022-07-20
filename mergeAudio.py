f997ade367b94988b5fab2c41267f18d04b671c0
t110368032HSU-ChienTe <93695115+t110368032HSU-ChienTe@users.noreply.github.com>
2022-07-20 08:18:14 +0800

    Add files via upload

--- mergeAudio.py
+++ mergeAudio.py
@@ -5,15 +5,26 @@ import os
 save_folder = "voice1_mergeAudio"
 data_folder = "voice1_cutAudio"
 
+speaker_num=0
+wav_num =0
 
 speakers = os.listdir(data_folder)
 
 for speaker in speakers:
+    #印出到第幾個人，哪一個人
+    speaker_num+=1
+    print("第{}個,語者:{}".format(speaker_num,speaker))
+
     if not os.path.exists("{}/{}".format(save_folder,speaker)):
         os.mkdir("{}/{}".format(save_folder,speaker))
     #看放wav的資料夾
     wavs_folder = os.listdir(data_folder+'/'+speaker)
+
     for wav_name_folder in wavs_folder:
+        #看第幾個wav檔
+        wav_num+=1
+        print("第{}個語音檔,檔案名{}".format(wav_num,wav_name_folder))
+
         #創建wav_folder
         if not os.path.exists("{}/{}/{}".format(save_folder,speaker,wav_name_folder)):
             os.mkdir("{}/{}/{}".format(save_folder,speaker,wav_name_folder))
